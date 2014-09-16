30d03abdfee4093863428c738bf76d84f25de35f
bootjp <bootjp@gmail.com>
2014-09-16 18:56:36 +0900

    Update useradd.sh

--- useradd.sh
+++ useradd.sh
@@ -6,21 +6,20 @@ if [ $# -ne 2 ]; then
  exit 1
 fi
 
-# ユーザー作成 				
+# ユーザー作成 ここではログイン不能ユーザの作成				
 /usr/sbin/useradd -s /sbin/nologin $1
 
 #ホームディレクトリのパーミッション変更		
 chmod 711 /home/$1
 
 # Disk quota のサンプルをコピー
-edquota -p smple $1
+edquota -p sample $1
 
 # パスワード設定		
 echo $1":"$2 | /usr/sbin/chpasswd
 
 # MySQL ユーザー設定
-mysql --user=root --password=[mysql root password] <<eof
+mysql --user=root --password=mysql root password <<eof
 create database $1;
 grant all on $1.* to $1@localhost identified by '$2';
 eof
-
