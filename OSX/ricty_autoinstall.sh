a791ba61762601b671d64ed8a8e59b959d040a06
bootjp <oh@bootjp.me>
2015-07-17 00:56:45 +0900

    add OSX files

---
+++ OSX/ricty_autoinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+brew install automake
+brew install pkg-config
+brew tap sanemat/font
+brew install Caskroom/cask/xquartz
+brew install ricty
+cp -f /usr/local/Cellar/ricty/3.2.4/share/fonts/Ricty*.ttf ~/Library/Fonts/
---
+++ OSX/ulimit_setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ulimit -u 65536 65536
+ulimit -n 65536 65536
