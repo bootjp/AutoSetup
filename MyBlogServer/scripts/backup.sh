d6f84b9c58a05b9e216e9884131654b1b4f7c07b
bootjp <oh@bootjp.me>
2015-12-13 00:12:53 +0900

    update letsencrypt, fastcgi_cache_lock

--- MyBlogServer/scripts/backup.sh
+++ MyBlogServer/scripts/backup.sh
@@ -15,3 +15,61 @@ git commit -m "backup$TODAY"
 git push origin backup
 
 echo 3 > /proc/sys/vm/drop_caches
+
+yum -y update
+yum -y update --enablerepo=remi --enablerepo=remi-php56
+
+cd /root/letsencrypt
+
+./letsencrypt-auto --renew certonly --webroot -d bootjp.me --webroot-path /var/www/bootjp.me/
+status=$?
+if [ $status -ne 0 ]; then
+  echo "letsencrypt bootjp.me Error!!"
+  exit 1
+fi
+
+nginx -t
+status=$?
+if [ $status -eq 0 ]; then
+  systemctl reload nginx
+  else
+  echo "Nginx Error!!"
+  exit 1
+fi
+
+yum -y update
+yum -y update --enablerepo=remi --enablerepo=remi-php56
+
+cd /root/letsencrypt
+
+./letsencrypt-auto --renew certonly --webroot -d bootjp.me --webroot-path /var/www/bootjp.me/
+status=$?
+if [ $status -ne 0 ]; then
+  echo "letsencrypt bootjp.me Error!!"
+  exit 1
+fi
+
+nginx -t
+status=$?
+if [ $status -eq 0 ]; then
+  systemctl reload nginx
+  else
+  echo "Nginx Error!!"
+  exit 1
+fi
+
+./letsencrypt-auto --renew certonly --webroot -d st.bootjp.me --webroot-path /var/www/bootjp.me/wp-content/uploads/
+status=$?
+if [ $status -ne 0 ]; then
+  echo "letsencrypt st.bootjp.me Error!!"
+  exit 1
+fi
+
+nginx -t
+status=$?
+if [ $status -eq 0 ]; then
+  systemctl reload nginx
+  else
+  echo "Nginx Error!!"
+  exit 1
+fi
