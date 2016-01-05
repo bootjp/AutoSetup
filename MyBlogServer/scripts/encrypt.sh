c0d358536019ecb49bfdce86da1bdff567c7c75d
bootjp <oh@bootjp.me>
2016-01-05 18:23:20 +0900

    script update

--- MyBlogServer/scripts/encrypt.sh
+++ MyBlogServer/scripts/encrypt.sh
@@ -3,8 +3,7 @@
 # 0 4 */10 * * /root/encrypt.sh
 
 cd /root/letsencrypt
-
-./letsencrypt-auto --renew certonly --webroot -d bootjp.me --webroot-path /var/www/bootjp.me/
+./letsencrypt-auto --renew certonly --webroot -d bootjp.me -d www.bootjp.me --webroot-path /var/www/bootjp.me/
 status=$?
 if [ $status -ne 0 ]; then
   echo "letsencrypt bootjp.me Error!!"
@@ -24,22 +23,6 @@ fi
 status=$?
 if [ $status -ne 0 ]; then
   echo "letsencrypt st.bootjp.me Error!!"
-  exit 1
-fi
-
-/usr/sbin/nginx -t
-status=$?
-if [ $status -eq 0 ]; then
-  systemctl reload nginx
-else
-  echo "Nginx Error!!"
-  exit 1
-fi
-
-./letsencrypt-auto --renew certonly --webroot -d www.bootjp.me --webroot-path /var/www/bootjp.me/
-status=$?
-if [ $status -ne 0 ]; then
-  echo "letsencrypt www.bootjp.me Error!!"
   exit 1
 fi
 
