875226d724eb1efd0757da646f9d7235c7fc6af5
bootjp <oh@bootjp.me>
2016-02-20 23:24:23 +0900

    upddate docker config

--- servers/scripts/docker_deploy.sh
+++ servers/scripts/docker_deploy.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# https://github.com/bootjp/AutoSetup/blob/master/servers/configs/etc/nginx/conf.d/docker_proxy.conf#L1-L5
+# https://github.com/bootjp/AutoSetup/blob/master/servers/configs/etc/nginx/conf.d/docker_proxy.conf#L1-L8
 
-docker rm -f $(docker ps | awk '/0.0.0.0:8080->80/ { print $1 }')
-docker run -pd 8080:80 -e CI_ENV='production' hogehoge
-docker rm -f $(docker ps | awk '/0.0.0.0:8000->80/ { print $1 }')
-docker run -pd 8000:80 -e CI_ENV='production' hogehoge
+docker rm -f $(docker ps | awk '/172.17.0.1:8080->80/ { print $1 }')
+docker run -pd 172.17.0.1:8080:80 -e CI_ENV='production' hogehoge
+docker rm -f $(docker ps | awk '/172.17.0.1:8000->80/ { print $1 }')
+docker run -pd 172.17.0.1:8000:80 -e CI_ENV='production' hogehoge
+docker rm -f $(docker ps | awk '/172.17.0.1:8800->80/ { print $1 }')
+docker run -pd 172.17.0.1:8800:80 -e CI_ENV='production' hogehoge
+docker rm -f $(docker ps | awk '/172.17.0.1:8888->80/ { print $1 }')
+docker run -pd 172.17.0.1:8888:80 -e CI_ENV='production' hogehoge
